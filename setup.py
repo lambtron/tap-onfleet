c023e3864058829644015149c66110b977d1a186
lambtron <andyjiang@gmail.com>
2019-03-27 23:43:31 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-onfleet',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the onfleet API',
       author='Stitch',
       url='http://github.com/lambtron/tap-onfleet',
